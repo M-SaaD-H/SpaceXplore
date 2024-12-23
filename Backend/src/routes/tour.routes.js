a4d1f85d77b65c8eb91fd4034c48f6305dc0cee9
Muhammad-Saad-Haider <mohd.saadhaider@gmail.com>
2024-12-23 13:24:36 +0530

    remove unused getAllAvailableDestinations route from tour routes

--- Backend/src/routes/tour.routes.js
+++ Backend/src/routes/tour.routes.js
@@ -7,6 +7,5 @@ const router = Router();
 router.route('/book-tour').post(verifyJWT, bookATour);
 router.route('/cancel-tour').post(verifyJWT, cancelTour);
 
-router.route('/get-all-destinations').get(getAllAvailableDestinations);
 
 export default router;

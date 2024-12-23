cb7fdb82def7d50e9b992de2595ac76b19163c80
Muhammad-Saad-Haider <mohd.saadhaider@gmail.com>
2024-12-23 12:22:52 +0530

    add getAllAvailableDestinations endpoint to fetch available destinations based on price and duration filters

--- Backend/src/routes/tour.routes.js
+++ Backend/src/routes/tour.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { bookATour, cancelTour } from "../controllers/tour.controller.js";
+import { bookATour, cancelTour, getAllAvailableDestinations } from "../controllers/tour.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route('/book-tour').post(verifyJWT, bookATour);
 router.route('/cancel-tour').post(verifyJWT, cancelTour);
+
+router.route('/get-all-destinations').get(getAllAvailableDestinations);
 
 export default router;

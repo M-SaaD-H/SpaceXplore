a89efeb4498af36048e334134c2ad4c9c5ea2430
Muhammad-Saad-Haider <mohd.saadhaider@gmail.com>
2025-01-29 00:46:46 +0530

    fix: correct static file serving path to use relative directory

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ const __dirname = path.dirname(__filename);
 
 app.set('views', path.join(__dirname, '../views'));
 
-app.use("/public", express.static('../public'));
+app.use("/public", express.static('public'));
 
 // app.get ("/",(req,res)=>{
 //     res.render("index")
